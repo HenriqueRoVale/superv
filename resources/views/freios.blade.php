f0f55bcccdae4893078f2aecd4d24e518a53d21f
Henrique Vale <60930357+HenriqueRoVale@users.noreply.github.com>
2020-05-09 16:00:12 -0300

    2.4

--- resources/views/freios.blade.php
+++ resources/views/freios.blade.php
@@ -1,13 +1,4 @@
-<?php
+<div id="poll_div"></div>
 
-<<<<<<< HEAD
-<div id="temps_div"></div>
-// With Lava class alias
-<?= Lava::render('LineChart', 'teste', 'temps_div') ?>
-
-// With Blade Templates
-@linechart('teste', 'temps_div')
-=======
 @linechart('teste','poll_div');
 {{-- Lava::render('BarChart', 'valor', 'poll_div') --}}
->>>>>>> 4fbc069c57b5221c58d40fe2931e9ec76ceefe91
